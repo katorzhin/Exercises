f9beef824ecc5a032f89e92ba56fd45da3b6fa99
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-11-15 22:41:02 +0200

    first task

---
+++ src/cycles/FactorialOfTheNumber.java
@@ -0,0 +1,21 @@
+package cycles;
+
+import java.util.Scanner;
+
+public class FactorialOfTheNumber {
+    public static int calculateFactorial(int number) {
+        int result = 1;
+        for (int i = 1; i < number; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number = scanner.nextInt();
+        System.out.println("Factorial of the number " + number + " = " + calculateFactorial(number));
+    }
+}
