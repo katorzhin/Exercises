c758926766cb1f652ef61b4a689264e62c7a51de
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-11-19 14:49:46 +0200

    task  functionValueTable and Sum and product of digits of a number

--- src/cycles/SumAndProductOfNumbers.java
+++ src/cycles/SumAndProductOfNumbers.java
@@ -1,13 +1,33 @@
 package cycles;
 
 public class SumAndProductOfNumbers {
-    public static void main(String[] args) {
-        int number = 325;
+    public static void sumOfNumber(int number) {
         int sum = 0;
-        while (number > 0) {
-            number = sum + number % 10;
-            sum = number / 10;
+        while (number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
         }
-        System.out.println(number);
+        System.out.println(sum);
+    }
+
+    public static void productOfNumbers(int number) {
+        int result = (number / 100) * (number / 10 % 10) * (number % 10);
+        System.out.println(result);
+    }
+
+    public static void functionValueTable(double x1,double x2,double up){
+        double y;
+        while (x1 <= x2) {
+            y = -0.23 * x1 * x1 + x1;
+            System.out.println(x1 + " -> " + y);
+            x1 = x1 + up;
+        }
+    }
+    public static void main(String[] args) {
+
+
+        sumOfNumber(3254);
+        productOfNumbers(123);
+
     }
 }
