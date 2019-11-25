7a5d737d25258cb681ce9743a89d5f7763048b04
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-11-25 23:42:48 +0200

    task strings

---
+++ src/strings/RemovingDuplicateCharacters.java
@@ -0,0 +1,42 @@
+package strings;
+
+public class RemovingDuplicateCharacters {
+    public static boolean contains(char[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String string = "cpu gpu mb cooler";
+
+        char[] a = string.toCharArray();
+        char[] b = new char[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (!contains(b, a[i])) {
+                b[i] = a[i];
+            }
+        }
+        String myStr = new String(b);
+        System.out.println(myStr);
+
+      while (myStr.contains(" ")){
+          String replace = myStr.replace(" ","");
+          myStr=replace;
+      }
+        System.out.println(myStr);
+
+
+//        System.out.println(Arrays.toString(b));
+//        String c = "a b c d e";
+//        String d = c.replaceAll(" ", "");
+//        System.out.println(d);
+
+    }
+
+}
+
+
---
+++ src/strings/SubstringReplace.java
@@ -0,0 +1,8 @@
+package strings;
+
+public class SubstringReplace {
+    public static void main(String[] args) {
+        String string = "Здравствуйте, Змей Горыныч. Сегодня совещание в 10.00";
+        System.out.println(string.replace("Змей","Хуй"));
+    }
+}
