8c0eb89330e839648e65d1a9132d63eb0672c4a3
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-12-02 23:42:23 +0200

    edit

--- src/polymorphism/ElectricBoogieDancer.java
+++ src/polymorphism/ElectricBoogieDancer.java
@@ -4,7 +4,6 @@ public class ElectricBoogieDancer extends Dancer {
     public ElectricBoogieDancer(String name, int age) {
         super(name, age);
     }
-// переопределение метода базового класса
     @Override
     public void dance() {
         System.out.println( toString() + "Я танцую электрик буги!");
@@ -16,7 +15,6 @@ class BreakDankDancer extends Dancer{
     public BreakDankDancer(String name, int age) {
         super(name, age);
     }
-// переопределение метода базового класса
     @Override
     public void dance(){
         System.out.println(toString() + "Я танцую брейк-данс!");
--- src/polymorphism/Main.java
+++ src/polymorphism/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Dancer dancer = new Dancer("Антон", 18);
 
-        Dancer breakDanceDancer = new BreakDankDancer("Алексей", 19);// восходящее преобразование к базовому типу
-        Dancer electricBoogieDancer = new ElectricBoogieDancer("Игорь", 20); // восходящее преобразование к базовому типу
+        Dancer breakDanceDancer = new BreakDankDancer("Алексей", 19);
+        Dancer electricBoogieDancer = new ElectricBoogieDancer("Игорь", 20);
 
 
         List<Dancer> discotheque = Arrays.asList(dancer, breakDanceDancer, electricBoogieDancer);
         for (Dancer d : discotheque) {
-            d.dance();// полиморфный вызов метода
+            d.dance();
         }
     }
 }
