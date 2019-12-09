10d5c36830e84e70196a7f254bccfcb0fd570445
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-12-10 00:22:13 +0200

    task Syracuse

---
+++ src/cycles/cycles2/HypothesisSyracuse.java
@@ -0,0 +1,23 @@
+package cycles.cycles2;
+
+public class HypothesisSyracuse {
+    public static void main(String[] args) {
+        int firstNumber = 777;
+        int secondNumber = 788;
+        int tmp;
+        for (int i = firstNumber; i <= secondNumber; i++) {
+            tmp = i;
+            while (tmp != 1) {
+                if (tmp % 2 == 0) {
+                    tmp = tmp / 2;
+                } else {
+                    tmp = (tmp * 3 + 1) / 2;
+
+                }
+                System.out.print(tmp + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
