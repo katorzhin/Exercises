5ef4329cd9159686ab5e0e36f0b7fe881f6883d4
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-12-10 19:47:10 +0200

    task count time (with seconds)

--- src/CounterTime.java
+++ src/CounterTime.java
@@ -1,16 +1,32 @@
 public class CounterTime {
-    public static void countTime(int number) {
-        int hours = number / 60;
-        int minutes = number - (60 * hours);
-        if (minutes % 60 == 0) {
-            System.out.println(hours + " hours.");
-        } else {
-            System.out.println(hours + " hours and " + minutes + " minutes.");
-        }
-    }
+//    public static String countTime(int number) {
+//        int hours = number / 60;
+//        int minutes = number - (60 * hours);
+//        if (minutes % 60 == 0) {
+//            return hours + " hours.";
+//        } else {
+//            return hours + " hours and " + minutes + " minutes.";
+//        }
+//    }
+    public static String countTime(int number) {
 
+        int seconds = number / 60;
+        int minutes = number - (60 * seconds);
+        int tmp= minutes%60;
+        int hours=number/3600;
+        if (minutes % 60 == 0) {
+            return seconds + " minutes.";
+        } else {
+            return seconds + " minutes and " + minutes + " seconds.";
+        }else if (number>3600){
+            return hours+" hours "+tmp+" minutes "+seconds+" seconds.";
+        }
+
+    }
     public static void main(String[] args) {
-        countTime(1000);
+//        System.out.println(countTime(4000));
+        int a = 66%60;
+        System.out.println(a);
     }
 }
 
