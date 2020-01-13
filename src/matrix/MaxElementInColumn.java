58b6cd4cd0e0b55d33ac35bf2579cc68256755f7
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-01-13 18:51:27 +0200

    task update

--- src/matrix/MaxElementInColumn.java
+++ src/matrix/MaxElementInColumn.java
@@ -11,23 +11,28 @@ public class MaxElementInColumn {
     }
 
     public static void maxColumnElement(int[][] array) {
-        int SumFirstColumn = 0;
-        int SumSecondColumn = 0;
-        int SumThirdColumn = 0;
+        int firstColumn = 0;
+        int secondColumn = 0;
+        int thirdColumn = 0;
 
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array[i].length - 1; j++) {
-                SumFirstColumn += array[i][0];
-                SumSecondColumn += array[i][1];
-                SumThirdColumn += array[i][2];
-                System.out.print("- ");
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][0] > firstColumn) {
+                    firstColumn = array[i][0];
+                } else if (array[i][1] > secondColumn) {
+                    secondColumn = array[i][1];
+                } else if (array[i][2] > thirdColumn) {
+                    thirdColumn = array[i][2];
+                }
             }
+            System.out.print("- ");
         }
-        System.out.print("\n" + SumFirstColumn + " " + SumSecondColumn + " " + SumThirdColumn);
+
+        System.out.print("\n" + firstColumn + " " + secondColumn + " " + thirdColumn);
     }
 
     public static void main(String[] args) {
-        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}};
+        int[][] array = {{1, 12, 3}, {4, 15, 6}, {17, 8, 9}, {10, 11, 12}, {3, 4, 5}};
         printMatrix(array);
         maxColumnElement(array);
 
