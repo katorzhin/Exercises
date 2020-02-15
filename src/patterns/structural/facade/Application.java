704cdb76dda641c11976ac3543a69430204aca16
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-02-15 23:56:00 +0200

    update

--- src/patterns/structural/facade/Application.java
+++ src/patterns/structural/facade/Application.java
@@ -6,6 +6,5 @@ public class Application {
     public static void main(String[] args) {
         VideoConversionFacade converter = new VideoConversionFacade();
         File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
-        // ...
     }
 }
