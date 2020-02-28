394e8e8ecdc81c894640ae994655adc75a4fb2b1
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-02-28 23:20:36 +0200

    add class SnakeInSquare

---
+++ src/SnakeInSquare.java
@@ -0,0 +1,45 @@
+public class SnakeInSquare {
+    public static void printMatrix(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void lineToRight(int[][] arraySquare, int a, int b, int length, int number) {
+        for (int i = 0; i < length; i++) {
+            arraySquare[a][b++] = number++;
+        }
+    }
+
+    public static void lineToDown(int[][] arraySquare, int a, int b, int length, int number) {
+        for (int i = 0; i < length; i++) {
+            arraySquare[a++][b] = number++;
+        }
+    }
+
+    public static void lineToLeft(int[][] arraySquare, int a, int b, int length, int number) {
+        for (int i = 0; i < length; i++) {
+            arraySquare[a][b--] = number++;
+        }
+    }
+
+    public static void lineToUp(int[][] arraySquare, int a, int b, int length, int number) {
+        for (int i = 0; i < length; i++) {
+            arraySquare[a--][b] = number++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+        int start = 0;
+        lineToRight(array, start, start, array.length, start + 1);
+        lineToDown(array, start + 1, array.length - 1, array.length - 1, array.length + 1);
+        lineToLeft(array, array.length - 1, array.length - 2, array.length - 1, array.length * 2);
+        lineToUp(array, array.length - 2, start, array.length - 2, array.length * 3 - 1);
+
+        printMatrix(array);
+    }
+}
