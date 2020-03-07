8017e1b2a0552f522cba64a5c7613e9f884a8ef0
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-03-07 23:53:55 +0200

    update

--- src/strings/CountLowercaseAndUppercase.java
+++ src/strings/CountLowercaseAndUppercase.java
@@ -8,7 +8,7 @@ public class CountLowercaseAndUppercase {
         for (int i = 0; i < string.length(); i++) {
             if (Character.isUpperCase(string.charAt(i))) {
                 countUppercase++;
-            } else if (Character.isLowerCase(string.charAt(i))){
+            } else if (Character.isLowerCase(string.charAt(i))) {
                 countLowercase++;
             }
         }
