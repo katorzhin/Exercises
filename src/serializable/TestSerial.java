5ce19a7d8bf281f9986c8030197abb3f5a1db989
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-03-11 23:59:39 +0200

    update

--- src/serializable/TestSerial.java
+++ src/serializable/TestSerial.java
@@ -3,13 +3,13 @@ package serializable;
 import java.io.*;
 
 class TestSerial implements Serializable {
-  public byte version = 100;
-  public byte count = 0;
+    public byte version = 100;
+    public byte count = 0;
 
-  public static void main(String args[]) throws IOException, ClassNotFoundException {
-    FileInputStream fis = new FileInputStream("temp.out");
-    ObjectInputStream oin = new ObjectInputStream(fis);
-    TestSerial ts = (TestSerial) oin.readObject();
-    System.out.println("version="+ts.version);
-  }
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream("temp.out");
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        TestSerial ts = (TestSerial) oin.readObject();
+        System.out.println("version =" + ts.version);
+    }
 }
