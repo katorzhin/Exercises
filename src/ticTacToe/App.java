a3640aa39c4c557c151cf402385da9dc8ff35edd
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-04-30 23:01:00 +0300

    added exceptions and space entry

--- src/ticTacToe/App.java
+++ src/ticTacToe/App.java
@@ -41,19 +41,22 @@ public class App {
             currentPlayer = 'X';
         }
     }
-
     public static void enterTurn(char[][] myArray, Scanner scanner) {
         System.out.println("TURN " + currentPlayer);
-        System.out.println("Please, enter the first index:");
-        int firstIndex = scanner.nextInt();
-//        if (firstIndex>3){
-//            System.out.println("fuck!");
-//            return;
-//        }
-        System.out.println("Enter the second index:");
-        int secondIndex = scanner.nextInt();
-
-        myArray[firstIndex][secondIndex] = currentPlayer;
+        while (true) {
+            String input = scanner.nextLine();
+            String[] coordinates = input.split(" ");
+            int secondIndex = Integer.parseInt(coordinates[1]);
+            int firstIndex = Integer.parseInt(coordinates[0]);
+            boolean badFirstInput = firstIndex >= myArray.length || firstIndex < 0;
+            boolean badSecondInput = secondIndex >= myArray.length || secondIndex < 0;
+            if (badFirstInput || badSecondInput || (myArray[firstIndex][secondIndex] == 'X' || myArray[firstIndex][secondIndex] == 'Y')) {
+                System.out.println("please correct index ");
+            } else {
+                myArray[firstIndex][secondIndex] = currentPlayer;
+                break;
+            }
+        }
 
 
     }
@@ -117,7 +120,6 @@ public class App {
         }
         return false;
     }
-
 
     public static void main(String[] args) {
         char[][] myArray = new char[3][3];
