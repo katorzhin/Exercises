104ae1e75bdd187df8aee38b69a7d2f9aa13c15e
alex katorzhin <alex.katorzhin@gmail.com>
2021-01-06 12:54:21 +0200

    added Part2

---
+++ src/pracrice1Epam/Part2.java
@@ -0,0 +1,13 @@
+package pracrice1Epam;
+
+public class Part2 {
+
+    public static int addition(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
